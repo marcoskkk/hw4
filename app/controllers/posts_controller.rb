059f2f96ff2788cc40cd553bdbb8df56722790e0
marcoskkk <marcoskenzo@gmail.com>
2022-03-14 20:15:24 +0000

    Authentication and Authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(params["post"])
+    @post.user_id = @current_user.id #set user_id in the database
     @post.save
     redirect_to "/places/#{@post.place.id}"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,33 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    entered_password = params["password"]
+
+    #checking the email
+    @user = User.find_by({email: entered_email})
+    
+    if @user # yes, email matches
+        #checking the password
+        if BCrypt::Password.new(@user.password) == entered_password
+            session["user_id"] = @user.id
+            redirect_to "/places"
+        else
+            #password doesn't match, send back to the login page
+            flash[:notice] = "Password doesn't match"
+            redirect_to "/sessions/new"
+        end
+    else
+        #email doesn't match, send back to the login page
+        flash[:notice] = "Email doesn't exist"
+        redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash[:notice] = "You have been logged out"
+    redirect_to "/sessions/new"
   end
 end
   
